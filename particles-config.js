abd3f32c2d6b7f91931b5a8f48396086b2a1d5d0
Jeferson Rocha <jefersonrocha92@gmail.com>
2024-12-15 14:25:32 -0300

    ajuste e melhorias no HTML e CSS

--- particles-config.js
+++ particles-config.js
@@ -15,30 +15,15 @@ particlesJS("particles-js", {
         stroke: {
           width: 0,
           color: "#000000"
-        },
-        polygon: {
-          nb_sides: 5
         }
       },
       opacity: {
         value: 0.5,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false
-        }
+        random: false
       },
       size: {
         value: 3,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 0.1,
-          sync: false
-        }
+        random: true
       },
       line_linked: {
         enable: true,
@@ -52,13 +37,7 @@ particlesJS("particles-js", {
         speed: 6,
         direction: "none",
         random: false,
-        straight: false,
-        out_mode: "out",
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200
-        }
+        out_mode: "out"
       }
     },
     interactivity: {
@@ -73,31 +52,6 @@ particlesJS("particles-js", {
           mode: "push"
         },
         resize: true
-      },
-      modes: {
-        grab: {
-          distance: 140,
-          line_linked: {
-            opacity: 1
-          }
-        },
-        bubble: {
-          distance: 400,
-          size: 40,
-          duration: 2,
-          opacity: 8,
-          speed: 3
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4
-        },
-        push: {
-          particles_nb: 4
-        },
-        remove: {
-          particles_nb: 2
-        }
       }
     },
     retina_detect: true
